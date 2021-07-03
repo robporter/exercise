efc49f41937845f6390f05abd5c1915109d8eabe
Rob Porter <rob.porter.work@gmail.com>
2021-07-03 17:03:44 +0100

    Refactored test to try some more sku codes

--- src/test/java/com/itv/checkout/domain/model/SkuTest.java
+++ src/test/java/com/itv/checkout/domain/model/SkuTest.java
@@ -1,14 +1,15 @@
 package com.itv.checkout.domain.model;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SkuTest {
 
-    @Test
-    void getCodeReturnsSkuCode() {
-        final String expectedSkuCode = "A";
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "B", "C", "D", "E"})
+    void getCodeReturnsSkuCode(String expectedSkuCode) {
         final Sku underTest = new Sku(expectedSkuCode);
 
         final String actual = underTest.getCode();
