b877092e0d707f2e72c9e9fd65d0a71200265d0c
Rob Porter <rob.porter.work@gmail.com>
2021-07-03 21:49:45 +0100

    Doc format

--- src/main/java/com/itv/checkout/domain/model/PricingRule.java
+++ src/main/java/com/itv/checkout/domain/model/PricingRule.java
@@ -4,7 +4,8 @@ public interface PricingRule {
     /**
      * Calculates a Pricing for a quantity of items.
      * @param quantity the number of items to price using this rule
-     * @return Pricing containing the summed price for the number of eligible units covered by this rule, and the number of units covered by this rule of those requested
+     * @return Pricing containing the summed price for the number of eligible units covered by this rule,
+     * and the number of units covered by this rule of those requested
      */
     Pricing getPricing(final int quantity);
 }
