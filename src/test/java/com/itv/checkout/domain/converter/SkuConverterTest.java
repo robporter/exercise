2041c2b996382ce2c05886d377c6ad342f1c3342
Rob Porter <rob.porter.work@gmail.com>
2021-07-04 12:49:11 +0100

    Refactored test to introduce parameters

--- src/test/java/com/itv/checkout/domain/converter/SkuConverterTest.java
+++ src/test/java/com/itv/checkout/domain/converter/SkuConverterTest.java
@@ -4,7 +4,13 @@ import com.itv.checkout.domain.model.Sku;
 import com.itv.checkout.persistence.entity.SkuEntity;
 import com.itv.checkout.persistence.entity.UnitCountPricingRuleEntity;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,10 +24,12 @@ class SkuConverterTest {
         underTest = new SkuConverter();
     }
 
-    @Test
-    void toEntity() {
+    @ParameterizedTest(name = "[{index}] skuCode: {0}, priceInPence: {1}")
+    @ArgumentsSource(SkuFixtures.class)
+    void toEntity(final String skuCode,
+                  final int priceInPence) {
 
-        final Sku sku = new Sku("A", 2);
+        final Sku sku = new Sku(skuCode, priceInPence);
 
         final SkuEntity actual = underTest.toEntity(sku);
 
@@ -35,4 +43,15 @@ class SkuConverterTest {
                 )
         );
     }
+
+    private static class SkuFixtures implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+        return Stream.of(
+                Arguments.of("A", 2),
+                Arguments.of("B", 3)
+        );
+    }
+}
 }
