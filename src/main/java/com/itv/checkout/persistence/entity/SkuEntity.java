349634007c62974ce0c5c92458819678d33cb8a6
Rob Porter <rob.porter.work@gmail.com>
2021-07-04 12:59:18 +0100

    Removed unused accessor on entity

--- src/main/java/com/itv/checkout/persistence/entity/SkuEntity.java
+++ src/main/java/com/itv/checkout/persistence/entity/SkuEntity.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 public class SkuEntity {
 
-    private final String code;
-    private final List<UnitCountPricingRuleEntity> unitCountPricingRuleEntities;
+    public final String code;
+    public final List<UnitCountPricingRuleEntity> unitCountPricingRuleEntities;
 
     public SkuEntity(final String code,
                      final List<UnitCountPricingRuleEntity> unitCountPricingRuleEntities) {
         this.code = code;
         this.unitCountPricingRuleEntities = unitCountPricingRuleEntities;
-    }
-
-    public List<UnitCountPricingRuleEntity> getUnitCountPricingRuleEntities() {
-        return unitCountPricingRuleEntities;
     }
 
     public String getCode() {
