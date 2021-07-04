51fc2f9cbbabfd2941a090237f69243e8f707917
Rob Porter <rob.porter.work@gmail.com>
2021-07-04 12:42:27 +0100

    Passing test for SkuConverter toEntity setting price information

--- src/main/java/com/itv/checkout/domain/converter/SkuConverter.java
+++ src/main/java/com/itv/checkout/domain/converter/SkuConverter.java
@@ -2,11 +2,20 @@ package com.itv.checkout.domain.converter;
 
 import com.itv.checkout.domain.model.Sku;
 import com.itv.checkout.persistence.entity.SkuEntity;
+import com.itv.checkout.persistence.entity.UnitCountPricingRuleEntity;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 public class SkuConverter {
+
     public SkuEntity toEntity(final Sku sku) {
-        return new SkuEntity(sku.getCode(), Collections.emptyList());
+        return new SkuEntity(
+                sku.getCode(),
+                Arrays.asList(new UnitCountPricingRuleEntity(
+                        sku.getPriceInPence(),
+                        1
+                ))
+        );
     }
+
 }
